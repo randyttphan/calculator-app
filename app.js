bbe7f059fd64ef07e17aa7398506385c2ae2c0a5
Randy Phan <randyphan@Randys-MacBook-Pro.local>
2020-01-12 17:21:38 -0600

    adding another checking for computing with multiple operands

--- app.js
+++ app.js
@@ -20,6 +20,9 @@ class Calculator {
 
   chooseOperation(operation) {
     if (this.currentOperand === "") return;
+    if (this.previousOperand !== "") {
+      this.compute();
+    }
     this.operation = operation;
     this.previousOperand = this.currentOperand;
     this.currentOperand = "";
