63b0504c5355f08db60f3c809851f05054acbfb2
Randy Phan <randyphan@Randys-MacBook-Pro.local>
2020-01-12 17:19:29 -0600

    appending numbers to update the display

---
+++ app.js
@@ -0,0 +1,58 @@
+class Calculator {
+  constructor(previousText, currentText) {
+    this.previousText = previousText;
+    this.currentText = currentText;
+    this.clear();
+  }
+
+  clear() {
+    this.currentOperand = "";
+    this.previousOperand = "";
+    this.operation = undefined;
+  }
+
+  delete() {}
+
+  appendNumber(number) {
+    if (number === "." && this.currentOperand.includes(".")) return;
+    this.currentOperand = this.currentOperand.toString() + number.toString();
+  }
+
+  chooseOperation(operation) {
+    if (this.currentOperand === "") return;
+    this.operation = operation;
+    this.previousOperand = this.currentOperand;
+    this.currentOperand = "";
+  }
+
+  compute() {}
+
+  updateDisplay() {
+    this.currentText.innerText = this.currentOperand;
+    this.previousText.innerText = this.previousOperand;
+  }
+}
+
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const equalsButton = document.querySelector("[data-equals]");
+const deleteButton = document.querySelector("[data-delete]");
+const clearButton = document.querySelector("[data-clear]");
+const previousText = document.querySelector("[data-previous-operand]");
+const currentText = document.querySelector("[data-current-operand]");
+
+const calculator = new Calculator(previousText, currentText);
+
+numberButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    calculator.appendNumber(button.innerText);
+    calculator.updateDisplay();
+  });
+});
+
+operationButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    calculator.chooseOperation(button.innerText);
+    calculator.updateDisplay();
+  });
+});
